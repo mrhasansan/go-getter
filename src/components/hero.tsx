fa1ec4aa879359d5ba55edf902fcb20174098c5a
Hasansan <marufhasansan@gmail.com>
2024-12-30 15:26:34 +0700

    change hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ export function Herocomponent() {
   return (
     <section className="bg-gray-50 py-12 ">
       <div className="container mx-auto flex flex-col lg:flex-row items-center px-6">
-        {/* Text Content */}
+        {/* Text Content
         <div className="lg:w-1/2 text-center lg:text-left">
           <h1 className="text-3xl font-bold text-gray-900 mb-4">👋 HELLO, GETTERS AND WELCOME TO:</h1>
           <h2 className="text-4xl font-bold text-yellow-500 mb-6">GO-GETTER ACADEMY</h2>
@@ -13,7 +13,22 @@ export function Herocomponent() {
           </p>
           <p className="text-gray-700 mb-6">Boost your IELTS skills and confidence with personalized classes and proven strategies! Whether you prefer one-on-one sessions or group learning, we’ve got you covered.</p>
           <button className="bg-yellow-500 text-white px-6 py-3 rounded-lg font-medium hover:bg-yellow-600">Find Out More</button>
-        </div>
+        </div> */}
+
+        <section className="bg-gray-100 py-12 px-6 text-center">
+          <div className="max-w-4xl mx-auto">
+            <h1 className="text-4xl font-bold text-logblue mb-4">
+              Go Get Your Dreams With <span className=" text-primary">Go-Getter Academy </span>
+            </h1>
+            <p className="text-lg text-gray-600 mb-6">Boost your IELTS skills and confidence with personalized classes and proven strategies! Whether you prefer one-on-one sessions or group learning, we’ve got you covered. </p>
+            <button className="bg-primary text-white py-2 px-6 rounded-lg hover:bg-yellow-500 transition">
+              <a href="https://www.instagram.com/gogetter_academy/" target="_blank" rel="noopener noreferrer">
+                {" "}
+                Find Out More
+              </a>
+            </button>
+          </div>
+        </section>
 
         {/* Image Content (Hidden on Small Devices) */}
         <div className="lg:w-1/2 mt-8 lg:mt-0  justify-center relative hidden lg:block">
@@ -24,20 +39,5 @@ export function Herocomponent() {
         </div>
       </div>
     </section>
-
-    // <section className="bg-gray-100 py-12 px-6 text-center">
-    //   <div className="max-w-4xl mx-auto">
-    //     <h1 className="text-4xl font-bold text-logblue mb-4">
-    //       Go Get Your Dreams With <span className=" text-primary">Go-Getter Academy </span>
-    //     </h1>
-    //     <p className="text-lg text-gray-600 mb-6">Boost your IELTS skills and confidence with personalized classes and proven strategies! Whether you prefer one-on-one sessions or group learning, we’ve got you covered. </p>
-    //     <button className="bg-primary text-white py-2 px-6 rounded-lg hover:bg-yellow-500 transition">
-    //       <a href="https://www.instagram.com/gogetter_academy/" target="_blank" rel="noopener noreferrer">
-    //         {" "}
-    //         Find Out More
-    //       </a>
-    //     </button>
-    //   </div>
-    // </section>
   );
 }
